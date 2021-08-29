abb70ba96ca73bfb681d7c147748b20f8eedf67e
Mikhail Coder <coder1coder@gmail.com>
2021-08-29 12:27:59 +0300

    избавился от слоя core

--- src/AwesomeShop.Core/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AwesomeShop.Core.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal Cost { get; set; }
-    }
-}
